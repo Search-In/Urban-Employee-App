55d5579b301c9fde94d2910df804b3481d370a07
Gopal Gawande <gopalgawande1605@gmail.com>
2024-09-19 18:26:09 +0530

    employe order handle scan bug resolved

--- src/Components/Employee/EmployeeOrder.jsx
+++ src/Components/Employee/EmployeeOrder.jsx
@@ -24,7 +24,6 @@ const EmployeeOrder = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const { orderId } = location.state || {}
-  console.log("Order Id from location is ", orderId)
 
   const [allProducts, setProducts] = useState([])
   const [openSnackbar, setOpenSnackbar] = useState(false)
@@ -152,22 +151,12 @@ const EmployeeOrder = () => {
 
   const handleScan = async (barcode) => {
     if (orderId) {
-      console.log("handlescan products ", allProducts)
-
       let foundProduct = false
       const updatedProducts = await Promise.all(
         allProducts.map(async (product) => {
-          console.log(
-            "first condition is Array.isArray(product?.productId?.barcode) ",
-            Array.isArray(product?.productId?.barcode)
-          )
-          console.log(
-            "second condtion is   product.productId.barcode.includes(barcode)",
-            product.productId.barcode.includes(barcode)
-          )
           if (
             Array.isArray(product?.productId?.barcode) &&
-            product.productId.barcode.includes(barcode)
+            product.productId.barcode.includes(String(barcode))
           ) {
             foundProduct = true
             if (product.scannedCount >= product.itemCount) {
